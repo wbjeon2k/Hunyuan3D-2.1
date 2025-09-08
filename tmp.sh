5f0ca3c82254f86bf1baee101366d99a206d3921
wbjeon2k <wbjeon2k@users.noreply.github.com>
2025-09-09 08:13:06 +0900

    initial commit

---
+++ tmp.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+pip install torch==2.5.1 torchvision==0.20.1 torchaudio==2.5.1 --index-url https://download.pytorch.org/whl/cu124
+pip install -r requirements.txt
+
+cd hy3dpaint/custom_rasterizer
+pip install -e .
+cd ../..
+cd hy3dpaint/DifferentiableRenderer
+bash compile_mesh_painter.sh
+cd ../..
+
+wget https://github.com/xinntao/Real-ESRGAN/releases/download/v0.1.0/RealESRGAN_x4plus.pth -P hy3dpaint/ckpt
