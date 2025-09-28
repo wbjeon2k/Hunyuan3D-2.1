4d192851598e74f72afeec6d1cd7e31af57352be
JWB <wbjeon2k@users.noreply.github.com>
2025-09-28 13:07:57 +0900

    0928 update train data pipeline

--- hy3dshape/tools/pipeline.sh
+++ hy3dshape/tools/pipeline.sh
@@ -1,9 +1,9 @@
 export OPENCV_IO_ENABLE_OPENEXR=1
-export OUTPUT_FOLDER=dataset/preprocessed
-export BLENDER_PATH=/path/to/blender-4.0.2-linux-x64/blender
+export OUTPUT_FOLDER=/home/test/Hunyuan3D-2.1/hy3dshape/tools/example_output
+export BLENDER_PATH=/home/test/Desktop/blender-4.5.3-linux-x64/blender
 
-export INPUT_FILE=dataset/raw/uuid.glb
-export NAME=uuid
+export INPUT_FILE=/home/test/Hunyuan3D-2.1/hy3dshape/tools/example_glb/Msh_BathroomSink.glb
+export NAME=example
 
 $BLENDER_PATH -b -P render/render.py -- --object ${INPUT_FILE} --output_folder $OUTPUT_FOLDER/$NAME/render_cond --geo_mode --resolution 512
 # $BLENDER_PATH -b -P render/render.py -- --object ${INPUT_FILE} --output_folder $OUTPUT_FOLDER/$NAME/render_tex --resolution 512
